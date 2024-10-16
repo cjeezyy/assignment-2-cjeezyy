706982c06f9a87ebdcb4ff0a596cdd5c69b80fc2
cjeezyy <cbj5193@psu.edu>
2024-10-16 17:35:25 -0400

    9/10 must pass invalid parameters

--- mdadm.c
+++ mdadm.c
@@ -9,9 +9,9 @@ int mount_count = 0; //Nothing has been mounted so mount_count = 0//setting coun
 uint32_t encode_op(int cmd, int disk_num, int block_num){
 
   uint32_t op = 0;
-  op |= cmd << 26;
-  op |= disk_num << 22;
-  op |= block_num<< 0;
+  op |= cmd << 12;
+  op |= disk_num << 0;
+  op |= block_num << 4;
 
   return op;
  
@@ -38,156 +38,70 @@ int mdadm_unmount(void) {
 
   uint32_t op = encode_op(JBOD_UNMOUNT,0,0); //initiate unmount disk command
   jbod_operation(op, NULL); //unmount disk
-  mount_count = 0;      //global variable changed to unmount
+  mount_count = 0;      //'global' variable changed to unmount
   if (mount_count == 0){return 1;} //if a disk is unmounted(mount_count =0), then unmount succeeds
   
   return -1;
 }
 
 int mdadm_read(uint32_t start_addr, uint32_t read_len, uint8_t *read_buf)  {
-  /* uint32_t disk_num;
-  uint32_t block_num;
-  uint32_t disk_offset;
-  uint32_t current_Addr = start_addr;
-  uint8_t myBuf[JBOD_BLOCK_SIZE] = ;
-  uint32_t len = read_len;
+   uint32_t disk_num, block_num, disk_offset, current_addr = start_addr;
+   uint8_t myBuf[JBOD_BLOCK_SIZE];
+   uint32_t len = read_len;
 
-   //check to see if disk is mounted
+  // Check if the disk is mounted  
+  if (mount_count == 0) {return -3;} //if disk is unmounted fail
 
-  if (mount_count == 0){return -1;}  //If the disk is not mounted read will fail
-  if (len > 1024){return -1;}        //If the specified length is not 1024 the read will fail
-  if ((buf == NULL) && (len == 0)){return len;}   //If buff is NULL and there is a length of 0 read will pass
-  if ((addr+len) > 1048576){return -1;}   //If the addr and the length of the read exceed the max bits the statement will fail
-  if (buf == NULL && addr >= 0 && len > 0) {return -1;}
+  //0-length read should succeed with a NULL pointer
+  if (len == 0 && read_buf == NULL) {return 1;}
+   
+  // Check for valid read length
+  if (len == 0 || len > 1024) {return -2;}
   
-
-
+  // Check for null buffer
+  if (read_buf == NULL) {return -4;} //if buffer space is null the disk read should fail
   
- 
-  disk_num = (addr / JBOD_DISK_SIZE);
-  disk_offset = (addr % JBOD_DISK_SIZE);
+  
+  
+  // Check if read exceeds maximum address space
+  if (start_addr + len > 1048576) {return -1;} //check that you have valid address space before disk read
+  
+  // Calculate disk, block, and offset
+  disk_num = (start_addr / JBOD_DISK_SIZE);
+  disk_offset = (start_addr % JBOD_DISK_SIZE);
   block_num = (disk_offset / JBOD_BLOCK_SIZE);
 
-  //Read from within the same block
-  if ((addr + len) < JBOD_BLOCK_SIZE){
-    
-    //SEEK TO INTENDED DISK & BLOCK
-     jbod_operation(encode_op(JBOD_SEEK_TO_DISK,disk_num,0), NULL); //seek to disk
+  while (len > 0) {
+    // Seek to the intended disk
+    jbod_operation(encode_op(JBOD_SEEK_TO_DISK, disk_num, 0), NULL);
 
-     jbod_operation(encode_op(JBOD_SEEK_TO_BLOCK,0,block_num), NULL); //seek to block
+    // Seek to the intended block
+    jbod_operation(encode_op(JBOD_SEEK_TO_BLOCK, 0, block_num), NULL);
 
-     
-    
-     jbod_operation(encode_op(JBOD_READ_BLOCK,0,0), myBuf);  // reading into mybuf
-     
+    // Read the block into myBuf
+    jbod_operation(encode_op(JBOD_READ_BLOCK, 0, 0), myBuf);
 
-     
-     memcpy(buf, (myBuf + addr), len);  //Copy bytes from mybuf to buf
+    // Calculate how many bytes we can read from this block
+    uint32_t bytes_to_read = JBOD_BLOCK_SIZE - (current_addr % JBOD_BLOCK_SIZE);
+    if (bytes_to_read > len) {bytes_to_read = len;}
 
-     return len;
+    // Copy bytes from myBuf to read_buf
+    memcpy(read_buf, myBuf + (current_addr % JBOD_BLOCK_SIZE), bytes_to_read);
+
+    // Update variables
+    current_addr += bytes_to_read;
+    read_buf += bytes_to_read;
+    len -= bytes_to_read;
+
+    // Move to the next block or disk
+    block_num++;
+    if (block_num >= (JBOD_DISK_SIZE / JBOD_BLOCK_SIZE)) {
+      block_num = 0;
+      disk_num++;
+    }
   }
 
-  //Reading Across blocks
-  else if ((int)disk_num == (int)((addr + len )/ JBOD_DISK_SIZE)){
-
-    int leftoverBytes = ((addr+len) - JBOD_BLOCK_SIZE);
-
-    //SEEK TO INTENDED DISK & BLOCK
-    jbod_operation(encode_op(JBOD_SEEK_TO_DISK,disk_num,0), NULL); //seek to disk
-
-    jbod_operation(encode_op(JBOD_SEEK_TO_BLOCK,0,block_num), NULL); //seek to block
-
-
-    jbod_operation(encode_op(JBOD_READ_BLOCK,0,0), myBuf);  // reading into mybuf
-
-    memcpy(buf, myBuf + (addr % JBOD_DISK_SIZE), (len - leftoverBytes));  //Copy bytes from mybuf to buf
-
-		 
-
-    while (leftoverBytes > 0){
-
-      //Updating lcal variables once after block is read
-      current_Addr += len - leftoverBytes;
-      disk_num = (current_Addr / JBOD_DISK_SIZE);
-      block_num = (current_Addr % JBOD_DISK_SIZE) / JBOD_BLOCK_SIZE;
-
-       //SEEK TO INTENDED DISK & BLOCK
-      jbod_operation(encode_op(JBOD_SEEK_TO_DISK,disk_num,0), NULL); //seek to disk
-
-      jbod_operation(encode_op(JBOD_SEEK_TO_BLOCK,0,block_num), NULL); //seek to block
-
-      jbod_operation(encode_op(JBOD_READ_BLOCK,0,0), myBuf);  // reading into mybuf
-
-      if (leftoverBytes >= JBOD_BLOCK_SIZE){
-
-	memcpy(buf + (current_Addr - addr), myBuf, JBOD_BLOCK_SIZE);
-	current_Addr--;
-      }
-      
-      else{
-	memcpy(buf + (current_Addr - addr), myBuf, leftoverBytes);
-      }
-
-      leftoverBytes -= JBOD_BLOCK_SIZE;
-    }
-
-    return len;
-
-    }
-
-
-  //Reading across disks
-  else {
-
-    int leftoverBytes = (addr + len - JBOD_DISK_SIZE *  (disk_num + 1));
-
-    //SEEK TO INTENDED DISK & BLOCK
-    jbod_operation(encode_op(JBOD_SEEK_TO_DISK,disk_num,0), NULL); //seek to disk
-
-    jbod_operation(encode_op(JBOD_SEEK_TO_BLOCK,0,block_num), NULL); //seek to block
-
-     
-    
-    jbod_operation(encode_op(JBOD_READ_BLOCK,0,0), myBuf);  // reading into mybuf
-     
-
-     
-    memcpy(buf, myBuf + (addr % (JBOD_DISK_SIZE - 1)), (len - leftoverBytes));  //Copy bytes from mybuf to buf
-
-    while (leftoverBytes > 0){
-
-      current_Addr += len - leftoverBytes;
-      disk_num = (current_Addr / JBOD_DISK_SIZE);
-      block_num = (current_Addr % JBOD_DISK_SIZE) / JBOD_BLOCK_SIZE;
-
-      //SEEK TO INTENDED DISK & BLOCK
-      jbod_operation(encode_op(JBOD_SEEK_TO_DISK,disk_num,0), NULL); //seek to disk
-
-      jbod_operation(encode_op(JBOD_SEEK_TO_BLOCK,0,block_num), NULL); //seek to block
-
-      jbod_operation(encode_op(JBOD_READ_BLOCK,0,0), myBuf);  // reading into mybuf
-
-      //copying bytes in buffer
-
-      if (leftoverBytes >= JBOD_BLOCK_SIZE){
-
-	memcpy(buf, myBuf + (current_Addr - addr), JBOD_BLOCK_SIZE);
-	current_Addr --;
-
-      }
-
-      else {
-
-	memcpy(buf + (current_Addr - addr), myBuf, leftoverBytes);
-      }
-      
-      leftoverBytes -= JBOD_BLOCK_SIZE;
-
-    }
-
-    return len;
-  }      
-  */
-  return -1;
+  return read_len;  // Return the number of bytes read
+  
 }
 
